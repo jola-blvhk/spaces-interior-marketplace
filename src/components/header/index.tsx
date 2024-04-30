0c9a16d7ba7504ee6856060c42323f21bc18736d
jola-blvhk <awujoolabello@gmail.com>
2024-04-30 16:21:33 +0100

    mobile header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,38 +1,56 @@
 import Image from "next/image";
 import React from "react";
-import Search from '../../assets/icons/search.svg'
-import Profile from '../../assets/icons/profile.svg'
-import Cart from '../../assets/icons/cart.svg'
+import Search from "../../assets/icons/search.svg";
+import Profile from "../../assets/icons/profile.svg";
+import Cart from "../../assets/icons/cart.svg";
+import ProfileGreen from "../../assets/icons/profile-green.svg";
+import CartGreen from "../../assets/icons/cart-green.svg";
+import Hamburger from "../../assets/icons/hamburger.svg";
 const Header = () => {
-  const isLoggedIn = true;
+  const isLoggedIn = false;
   return (
     <div className="padding-section py-6 md:py-10 ">
       <div className="max-width-section flex justify-between items-center">
         <div>
-          <h2 className="hidden md:block text-primary-black-90">Consult with us</h2>
+          <Image className="md:hidden w-7 h-7 " src={Hamburger} alt="menu" />
+          <h2 className="hidden md:block text-primary-black-90">
+            Consult with us
+          </h2>
         </div>
-        <div className="text-center">
+        {/* logo  */}
+        <div className="text-center ml-8 md:ml-10">
           <h2 className=" font-megrim  text-4xl font-bold tracking-tight text-secondary-green-100">
             SPACES
           </h2>
           <p className="text-xs text-primary-black-90">Interior Market Place</p>
         </div>
-        {isLoggedIn && (
-          <div className="flex items-center justify-between gap-[49px]">
-            <Image className="w-7 h-7 " src={Search} alt="search" />
-            <Image className="w-7 h-7" src={Profile} alt="profile" />
-            <Image className="w-7 h-7" src={Cart} alt="cart" />
 
+        {/* mobile profile and cart */}
+        <div className="block md:hidden">
+          <div className="flex items-center justify-between gap-3">
+            <Image className="w-7 h-7 " src={ProfileGreen} alt="profile" />
+            <Image className="w-7 h-7" src={CartGreen} alt="cart" />
           </div>
-        )}
-        {!isLoggedIn && (
-          <div className="flex items-center justify-between gap-10">
-            <button>Log in</button>
-            <button className="border-2 border-black  bg-black text-white py-[14px] px-[28.5px] w-full text-center md:max-w-fit rounded-[15px] hover:bg-white hover:text-black">
-              Sign Up
-            </button>
-          </div>
-        )}
+        </div>
+
+        {/*desktop profile, cart and auth */}
+        <div className="hidden md:block">
+          {isLoggedIn && (
+            <div className="flex items-center justify-between gap-[49px]">
+              <Image className="w-7 h-7 " src={Search} alt="search" />
+              <Image className="w-7 h-7" src={Profile} alt="profile" />
+              <Image className="w-7 h-7" src={Cart} alt="cart" />
+            </div>
+          )}
+          {!isLoggedIn && (
+            <div className="flex items-center justify-between gap-10">
+              <button>Log in</button>
+              <button className="border-2 border-black  bg-black text-white py-[14px] px-[28.5px] w-full text-center md:max-w-fit rounded-[15px] hover:bg-white hover:text-black">
+                Sign Up
+              </button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
