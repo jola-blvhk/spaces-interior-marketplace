4bf9413eb9d1be4df53ad176f7d64f6f5d6b7d37
jola-blvhk <awujoolabello@gmail.com>
2024-05-02 12:13:34 +0100

    landing page first section

--- src/components/landing-page/first-section.tsx
+++ src/components/landing-page/first-section.tsx
@@ -10,10 +10,10 @@ const FirstSection = () => {
     setSearchValue(e.target.value);
   };
   return (
-    <div className="padding-section py-4 bg-primary-white-100">
+    <div className="padding-section pt-4 md:pt-6 lg:pt-12 bg-primary-white-100">
       <div className="max-width-section grid w-full justify-center">
-        <div className=" rounded-[10px] md:rounded-[15px] flex items-center gap-2 py-3 px-4 md:px-6  bg-primary-white-100 border border-solid border-[##D9D9D9] w-[350px] max-w-[400px] md:w-[420px] md:max-w-[420px] box-border rotate-[0.02deg]">
-          <IoSearch className="text-[#737373B2] text-2xl md:text-3xl" />
+        <div className=" rounded-[10px] md:rounded-[15px] flex m-auto items-center gap-2 py-3 px-4 md:px-6  bg-primary-white-100 border border-solid border-[##D9D9D9] w-[350px] max-w-[400px] md:w-[420px] md:max-w-[420px] box-border rotate-[0.02deg]">
+          <IoSearch className="text-[#737373B2] text-3xl" />
           <input
             placeholder="Search for furniture, accessories and more"
             defaultValue={searchValue}
@@ -21,6 +21,18 @@ const FirstSection = () => {
             // onClick={handleSearchClicked}
             onChange={handleChange}
           />
+        </div>
+
+              
+        <div className="text-center pt-7 ">
+          <h1 className="text-secondary-green-100 text-xl md:text-3xl lg:text-4xl font-medium leading-[30px]md:leading-[33px] lg:leading-[55px]">
+            Shop for latest furniture and home accessories
+            <br /> from your favourite vendors
+          </h1>
+          <h3 className="pt-3 lg:pt-0 text-sm text-primary-black-90 md:text-base lg:text-lg">
+            Your one stop market place for all things furniture, accessories and
+            more
+          </h3>
         </div>
       </div>
     </div>
