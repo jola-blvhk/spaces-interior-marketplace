b80ec6a1a39fe2e22ecd1366d2f9c0ebf3863299
jola-blvhk <awujoolabello@gmail.com>
2024-05-06 11:45:58 +0100

    categories page

--- src/components/back-button/index.tsx
+++ src/components/back-button/index.tsx
@@ -8,7 +8,7 @@ const BackButton = () => {
   };
   return (
     <div
-      className=" bg-secondary-green-100/10 border border-[#D9D9D9] rounded-[10px] cursor-pointer  md:rounded-[15px] w-fit p-4 md:p-5 hover:bg-secondary-green-100/80 font-bold focus:bg-secondary-green-100/80 focus:text-white  hover:text-white"
+      className=" bg-secondary-green-100/10 border border-[#D9D9D9] rounded-[10px] cursor-pointer  md:rounded-[15px] w-fit p-4 md:p-5 hover:bg-secondary-green-100/60 font-bold focus:bg-secondary-green-100/60 focus:text-white  hover:text-white"
       onClick={handleBack}
     >
       <IoChevronBackOutline className="lg:text-lg" />
