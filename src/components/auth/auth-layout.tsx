4e3c7af2acc47fb6eb116b88aa5affdb84512be9
jola-blvhk <awujoolabello@gmail.com>
2024-05-09 22:32:34 +0100

    authentication UI

--- src/components/auth/auth-layout.tsx
+++ src/components/auth/auth-layout.tsx
@@ -22,8 +22,8 @@ const AuthLayout = () => {
   ];
   const dispatch = useAppDispatch();
   return (
-    <div className="padding-section">
-      <div className="h-screen  max-w-[400px] m-auto ">
+    <div className="padding-section h-screen">
+      <div className="  max-w-[400px] h-fit my-auto ">
         <div className="m-auto  text-center w-fit mb-10 md:mb-11">
           <h2 className=" font-megrim text-3xl  md:text-4xl  lg:text-5xl font-extrabold tracking-tight text-secondary-green-100 ">
             SPACES
