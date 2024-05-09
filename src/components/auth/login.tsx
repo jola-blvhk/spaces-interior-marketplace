c963699d7f44d59d80da0fd0135e213c615070c5
jola-blvhk <awujoolabello@gmail.com>
2024-05-09 21:47:43 +0100

    authentication UI

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import React from "react";
-import AuthLayout from "./auth-layout";
+import InputField from "../input/input";
 
 const Login = () => {
-  return <div className="m-auto h-screen ">Login</div>;
+  return (
+    <form className="m-auto h-screen space-y-6  mt-12 md:mt-14 ">
+      <InputField label="Email Address" />
+      <InputField label="Password" isPassword/>
+    </form>
+  );
 };
 
 export default Login;
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,24 +1,45 @@
-import React, { InputHTMLAttributes } from "react";
-
+import React, { InputHTMLAttributes, useState } from "react";
+import { BiHappyHeartEyes } from "react-icons/bi";
+import { PiSmileyXEyesBold } from "react-icons/pi";
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
+  isPassword?: boolean;
 }
 
 const InputField: React.FC<InputFieldProps> = ({
   label,
-
+  isPassword = false,
   ...props
 }) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2">
-        {label}
-      </label>
-      <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        type="text"
-        {...props}
-      />
+    <div className="text-primary-black-90">
+      <label className="block text-sm mb-3">{label}</label>
+      <div className="relative">
+        <input
+          className="text-sm md:text-base tracking-wide bg-white focus-within:bg-white appearance-none border border-[#D9D9D9] rounded-[11px] w-full md:rounded-[15px]  px-3 py-4 leading-tight focus:outline-none focus:shadow-outline"
+          type={isPassword ? (!showPassword ? "password" : "text") : "text"}
+          {...props}
+        />
+        {isPassword && (
+          <button
+            className="absolute inset-y-0 right-0 flex items-center px-3 text-primary-black-90"
+            onClick={togglePasswordVisibility}
+            tabIndex={-1}
+          >
+            {showPassword ? (
+              <PiSmileyXEyesBold className="h-6 w-6 text-secondary-green-100" />
+            ) : (
+              <BiHappyHeartEyes className="h-6 w-6 text-secondary-green-100" />
+            )}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
