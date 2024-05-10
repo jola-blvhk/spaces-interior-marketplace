f28b85f05bc2a7fc2076a2e1f619dcb836ef9dd9
jola-blvhk <awujoolabello@gmail.com>
2024-05-10 13:30:15 +0100

    authentication

--- src/components/auth/signup.tsx
+++ src/components/auth/signup.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import InputField from "../input/input";
 import Button from "../button";
+import ThirdPartyAuth from "./third-party-auth";
 
 const SignUp = () => {
   return (
     <div>
-      {" "}
-      <form className="m-auto h-fit space-y-6 md:space-y-8  mt-12 md:mt-14 ">
+      <form className="m-auto h-fit space-y-6 md:space-y-8  mt-6 md:mt8 ">
+        <ThirdPartyAuth />
         <InputField label="Email Address" placeholder="xyz@gmail.com" />
         <InputField label="First Name" placeholder="first name" />
         <InputField label="Last Name" placeholder="last name" />
