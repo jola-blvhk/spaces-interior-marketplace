f431dc91a70c618b298b32155f2ffa5a50e99a0e
jola-blvhk <awujoolabello@gmail.com>
2024-05-10 15:00:36 +0100

    authentication

--- src/components/auth/auth-layout.tsx
+++ src/components/auth/auth-layout.tsx
@@ -6,6 +6,8 @@ import VendorProductFilter from "../filter/vendor-product";
 import { authPageActions } from "@/redux/auth-page-slice";
 import SignUp from "./signup";
 import Login from "./login";
+import { ROUTES } from "@/app/utils/routes";
+import Link from "next/link";
 
 const AuthLayout = () => {
   // const [selectedSection, setSelectedSection] = useState<string>("login");
@@ -24,20 +26,24 @@ const AuthLayout = () => {
   return (
     <div className="padding-section pt-12 grid items-center md:h-screen">
       <div className="  max-w-[450px] h-fit   w-full m-auto my-auto ">
-        <div className="m-auto  text-center w-fit mb-10 md:mb-11">
-          <h2 className=" font-megrim text-3xl  md:text-4xl  lg:text-5xl font-extrabold tracking-tight text-secondary-green-100 ">
-            SPACES
-          </h2>
-          <p className=" text-[10px] md:text-xs lg:text-base text-primary-black-90">
-            Interior Market Place
-          </p>
+        <div className="">
+          <Link
+            href={ROUTES.Home}
+            className="m-auto text-center w-fit mb-10 md:mb-11"
+          >
+            <h2 className=" font-megrim text-3xl  md:text-4xl  lg:text-5xl font-extrabold tracking-tight text-secondary-green-100 ">
+              SPACES
+            </h2>
+            <p className=" text-[10px] md:text-xs lg:text-base text-primary-black-90">
+              Interior Market Place
+            </p>
+          </Link>
+          <VendorProductFilter
+            selectedSection={authPageState}
+            onSectionClick={handleSectionClick}
+            sections={sections}
+          />
         </div>
-        <VendorProductFilter
-          selectedSection={authPageState}
-          onSectionClick={handleSectionClick}
-          sections={sections}
-        />
-
         {authPageState === "signup" && <SignUp />}
 
         {authPageState === "login" && <Login />}
