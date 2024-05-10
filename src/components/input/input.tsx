cf5c1ed5bc7b4362b5b02a3d1e2cced37ee14cea
jola-blvhk <awujoolabello@gmail.com>
2024-05-10 12:04:15 +0100

    authentication

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -22,7 +22,7 @@ const InputField: React.FC<InputFieldProps> = ({
       <label className="block text-sm mb-1 md:mb-3">{label}</label>
       <div className="relative">
         <input
-          className="text-sm md:text-base tracking-wide bg-white focus-within:bg-white appearance-none border border-[#D9D9D9] rounded-[11px] w-full md:rounded-[15px]  px-3 py-3 md:py-4 leading-tight focus:outline-none focus:shadow-outline"
+          className="text-sm md:text-base tracking-wide bg-white focus-within:bg-white appearance-none border border-[#D9D9D9] rounded-[11px] w-full md:rounded-[15px]  px-3 py-3  leading-tight focus:outline-none focus:shadow-outline"
           type={isPassword ? (!showPassword ? "password" : "text") : "text"}
           {...props}
         />
@@ -30,7 +30,7 @@ const InputField: React.FC<InputFieldProps> = ({
           <button
             className="absolute inset-y-0 right-0 flex items-center px-3 text-primary-black-90"
             onClick={togglePasswordVisibility}
-            tabIndex={-1}
+           
           >
             {showPassword ? (
               <PiSmileyXEyesBold className="h-6 w-6 text-secondary-green-100" />
