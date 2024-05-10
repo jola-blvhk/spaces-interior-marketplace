13edc84c851f45412d39104604e2c3dc5020a909
jola-blvhk <awujoolabello@gmail.com>
2024-05-10 13:26:36 +0100

    authentication

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -9,12 +9,14 @@ const Login = () => {
   return (
     <>
       <form className="m-auto h-fit space-y-6 md:space-y-8  mt-12 md:mt-14 ">
-        <InputField label="Email Address" />
-        <InputField label="Password" isPassword />
+        <InputField label="Email Address" placeholder="xyz@example.com" />
+        <InputField label="Password" isPassword placeholder="**********" />
         <Button title="Log in" onclick={() => {}} blackBackground />
       </form>
 
-      <p className="text-center text-xs md:text-sm lg:text-base mt-4 md:mt-6 ">Forgot password?</p>
+      <p className="text-center text-xs md:text-sm lg:text-base mt-4 md:mt-6 ">
+        Forgot password?
+      </p>
       {/* Third Party Authentication */}
       <div className="mt-10 md:mt-14">
         <ThirdPartyAuth />
--- src/components/auth/signup.tsx
+++ src/components/auth/signup.tsx
@@ -7,11 +7,15 @@ const SignUp = () => {
     <div>
       {" "}
       <form className="m-auto h-fit space-y-6 md:space-y-8  mt-12 md:mt-14 ">
-        <InputField label="Email Address" />
-        <InputField label="First Name" />
-        <InputField label="Last Name" />
-        <InputField label="Password" isPassword />
-        <InputField label="Confirm Password" isPassword />
+        <InputField label="Email Address" placeholder="xyz@gmail.com" />
+        <InputField label="First Name" placeholder="first name" />
+        <InputField label="Last Name" placeholder="last name" />
+        <InputField label="Password" isPassword placeholder="**********" />
+        <InputField
+          label="Confirm Password"
+          isPassword
+          placeholder="**********"
+        />
         <Button title="Sign Up" onclick={() => {}} blackBackground />
       </form>
     </div>
