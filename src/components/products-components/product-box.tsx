83bf6bc38c2ae1aae4030cf8da406a8c920d40c0
jola-blvhk <awujoolabello@gmail.com>
2024-05-13 12:23:19 +0100

    top products

--- src/components/products-components/product-box.tsx
+++ src/components/products-components/product-box.tsx
@@ -26,12 +26,12 @@ const ProductBox: React.FC<ProductBoxProps> = ({
       </div>
       <div className="flex justify-between gap-5 md:gap-8 lg:gap-12 mt-3 md:mt-4 lg:mt-[18px]">
         <div>
-          <h3 className="line-clamp-2 text-sm md:text-base">{name}</h3>
-          <p className=" line-clamp-1 md:pt-1 text-[#C6C6C6] text-xs md:text-sm">
+          <h3 className="line-clamp-2 text-sm md:text-base lg:text-lg">{name}</h3>
+          <p className=" line-clamp-1 pt-1 text-[#C6C6C6] text-xs md:text-sm lg:text-base">
             {desc}
           </p>
         </div>
-        <h3 className="text-base md:text-lg ">₦{price}</h3>
+        <h3 className="text-base md:text-xl  lg:text-xl xl:text-2xl">₦{price}</h3>
       </div>
     </div>
   );
