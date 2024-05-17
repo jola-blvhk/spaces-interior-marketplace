290f0f23df8f9fd365f5abd9c01563273c8ecd1f
jola-blvhk <awujoolabello@gmail.com>
2024-05-17 10:36:22 +0100

    filter for category

--- src/components/filter/type-filter.tsx
+++ src/components/filter/type-filter.tsx
@@ -7,10 +7,10 @@ interface TypeFillerProps {
   types: string[];
 }
 const TypeFilter: React.FC<TypeFillerProps> = ({ types }) => {
-  const [clickedHeader, setClickedHeader] = useState(0);
+  const [clickedHeader, setClickedHeader] = useState<number>(0);
 
-  const handleHeaderClick = (index: any) => {
-    setClickedHeader(index === clickedHeader ? null : index);
+  const handleHeaderClick = (index: number) => {
+    setClickedHeader(index === clickedHeader ? 0 : index);
   };
   return (
     <div className="flex gap-2 md:gap-3  justify-between  whitespace-nowrap">
@@ -20,7 +20,7 @@ const TypeFilter: React.FC<TypeFillerProps> = ({ types }) => {
             key={index}
             onClick={() => handleHeaderClick(index)}
             className={` text-primary-white-100  py-2 px-7 lg:py-3 lg:px-10 text-sm lg:text-base rounded-3xl lg:rounded-[50px] cursor-pointer ${
-              clickedHeader
+              clickedHeader === index
                 ? " bg-secondary-green-100"
                 : "bg-secondary-green-100/50  hover:bg-secondary-green-100"
             }`}
