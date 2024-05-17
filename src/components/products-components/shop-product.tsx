c798182bf59fc7f005ad6ce92539f4e430af3d50
jola-blvhk <awujoolabello@gmail.com>
2024-05-17 20:17:27 +0100

    shop product page

--- src/components/products-components/shop-product.tsx
+++ src/components/products-components/shop-product.tsx
@@ -10,21 +10,43 @@ interface ShopProductProps {
 }
 const ShopProduct: React.FC<ShopProductProps> = ({ title }) => {
   const variations = [1, 2, 3];
+  const colors = [
+    {
+      name: "blue",
+      color: "#0A66C2",
+    },
+    {
+      name: "pink",
+      color: "#C837AB",
+    },
+    {
+      name: "red",
+      color: "#C5221F",
+    },
+    {
+      name: "green",
+      color: "#00FF00",
+    },
+  ];
   const [isHeartClicked, setIsHeartClicked] = useState(false);
-
+  const [clickedColor, setClickedColor] = useState<number>(0);
   const handleHeartClick = () => {
     setIsHeartClicked(!isHeartClicked);
   };
+
+  const handleColorClick = (index: number) => {
+    setClickedColor(index === clickedColor ? 0 : index);
+  };
   return (
     <div className="padding-section">
-      <div className="max-width-section">
+      <div className="max-width-section ">
         <TitleHeading
           title={title}
           description="Your one stop market place for all things furniture, accessories and more"
           showBackButton={true}
         />
 
-        <div className="grid lg:grid-cols-2 gap-10 mt-7 md:mt-16 lg:mt-24">
+        <div className="grid lg:grid-cols-2 gap-10 mt-7 md:mt-16 lg:mt-24 text-primary-black-90">
           <div className="flex gap-[2%]">
             <div className="w-[23%] grid grid-rows-3  gap-y-3">
               {variations?.map((variation, index) => (
@@ -69,6 +91,41 @@ const ShopProduct: React.FC<ShopProductProps> = ({ title }) => {
               Your one stop market place for all things furniture, accessories
               and more. Your one stop market place for all things furniture.
             </p>
+            <h3 className="font-semibold text-sm leading-6 lg:text-base">
+              Delivery duration: <span className="font-normal">2 weeks</span>
+            </h3>
+            <div className="grid md:flex justify-between ">
+              <div>
+                <h2>Variation</h2>
+                <div className="flex gap-3 md:gap-5">
+                  {colors?.map((color, index) => (
+                    <div
+                      key={index}
+                      onClick={() => {
+                        handleColorClick(index);
+                      }}
+                      className={`rounded-full flex justify-center items-center w-8 h-8 md:w-10 md:h-10 bg-white  p-[2px] md:p-1  z-0`}
+                      style={{
+                        border: ` 3px solid ${
+                          clickedColor === index ? color.color : "white"
+                        }`,
+                      }}
+                    >
+                      <div
+                        className="w-full h-full  m-auto border-3 border-white rounded-full cursor-pointer "
+                        style={{
+                          backgroundColor: color.color,
+                          border: clickedColor === index ? "white" : "none",
+                        }}
+                      >
+                        {" "}
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div>Size</div>
+            </div>
           </div>
         </div>
       </div>
