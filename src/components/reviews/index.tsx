75be2edea4be3d33e2ba441e1babf24b49a7616d
jola-blvhk <awujoolabello@gmail.com>
2024-05-21 14:25:39 +0100

    reviews

--- src/components/reviews/index.tsx
+++ src/components/reviews/index.tsx
@@ -1,9 +1,13 @@
-import { useAppDispatch, useAppSelector } from "@/redux";
+import { useAppDispatch } from "@/redux";
 import { reviewsComponentActions } from "@/redux/reviews-slice";
 import React, { useState } from "react";
 import "animate.css";
 import "animate.css/animate.min.css";
-import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
+import Draggable, {
+  DraggableData,
+  DraggableEvent,
+  DraggableCore,
+} from "react-draggable";
 
 const Reviews = () => {
   const dispatch = useAppDispatch();
@@ -12,49 +16,39 @@ const Reviews = () => {
 
   // State to track the height of the draggable div
   const [height, setHeight] = useState<string>("35%");
-  const [yPosition, setYPosition] = useState<number>(0);
 
   // Function to handle drag stop
   const handleDragStop = (e: DraggableEvent, data: DraggableData) => {
     const windowHeight = window.innerHeight;
-    const dragY = data.y + yPosition;
+    const finalYPosition = data.y + data.node.offsetHeight; // Calculate the final Y position
 
-    if (dragY < -windowHeight / 2) {
+    if (finalYPosition < windowHeight / 2) {
       setHeight("100%");
-      setYPosition(-windowHeight); // Set the y position to the top of the screen
     } else {
       setHeight("35%");
-      setYPosition(0); // Reset the y position to the original
     }
   };
 
-  // Function to handle drag
-  const handleDrag = (e: DraggableEvent, data: DraggableData) => {
-    setYPosition(data.y);
-  };
   return (
     <div className="relative">
-      <div className="fixed top-0 bottom-0 left-0 right-0 z-[1000000000000] block md:flex ">
+      <div className="fixed top-0 bottom-0 left-0 right-0 z-[1000000000000] block md:flex">
         <div
-          className="  w-screen h-screen  cursor-pointer bg-primary-black-100 opacity-30"
+          className="w-screen h-screen cursor-pointer bg-primary-black-100 opacity-30"
           onClick={() => {
             dispatch(setReviewsComponentState(false));
           }}
         ></div>
-        <Draggable
-          axis="y"
-          bounds="parent"
-          onDrag={handleDrag}
-          onStop={handleDragStop}
-        >
-          <div
-            className="absolute block md:hidden bottom-0 w-screen animate__animated animate__slideInUp bg-primary-white-100 transition-all duration-300 ease-in-out"
-            style={{ height }}
-          >
-            <h1>ReviewsPage</h1>
-          </div>
-        </Draggable>
-        <div className=" hidden md:block absolute right-0 h-screen md:w-[50%] lg:w-[35%] xl:w-[25%] animate__animated animate__slideInRight  bg-primary-white-100">
+        <DraggableCore>
+          <Draggable axis="y" bounds="parent" onStop={handleDragStop}>
+            <div
+              className="absolute block md:hidden bottom-0 w-screen animate__animated animate__slideInUp bg-primary-white-100 transition-all duration-300 ease-in-out"
+              style={{ height }}
+            >
+              <h1>ReviewsPage</h1>
+            </div>
+          </Draggable>
+        </DraggableCore>
+        <div className="hidden md:block absolute right-0 h-screen md:w-[50%] lg:w-[35%] xl:w-[25%] animate__animated animate__slideInRight bg-primary-white-100">
           <h1>ReviewsPage</h1>
         </div>
       </div>
