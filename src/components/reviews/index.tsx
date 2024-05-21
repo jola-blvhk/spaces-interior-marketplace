02c02ef2448e7e08ce454ea499b49f73d5fbbb94
jola-blvhk <awujoolabello@gmail.com>
2024-05-21 14:14:23 +0100

    reviews

--- src/components/reviews/index.tsx
+++ src/components/reviews/index.tsx
@@ -3,7 +3,7 @@ import { reviewsComponentActions } from "@/redux/reviews-slice";
 import React, { useState } from "react";
 import "animate.css";
 import "animate.css/animate.min.css";
-import Draggable, {DraggableData, DraggableEvent} from "react-draggable";
+import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
 
 const Reviews = () => {
   const dispatch = useAppDispatch();
@@ -12,14 +12,25 @@ const Reviews = () => {
 
   // State to track the height of the draggable div
   const [height, setHeight] = useState<string>("35%");
+  const [yPosition, setYPosition] = useState<number>(0);
 
   // Function to handle drag stop
   const handleDragStop = (e: DraggableEvent, data: DraggableData) => {
-    if (data.y < -window.innerHeight / 2) {
+    const windowHeight = window.innerHeight;
+    const dragY = data.y + yPosition;
+
+    if (dragY < -windowHeight / 2) {
       setHeight("100%");
+      setYPosition(-windowHeight); // Set the y position to the top of the screen
     } else {
       setHeight("35%");
+      setYPosition(0); // Reset the y position to the original
     }
+  };
+
+  // Function to handle drag
+  const handleDrag = (e: DraggableEvent, data: DraggableData) => {
+    setYPosition(data.y);
   };
   return (
     <div className="relative">
@@ -30,7 +41,12 @@ const Reviews = () => {
             dispatch(setReviewsComponentState(false));
           }}
         ></div>
-        <Draggable axis="y" bounds="parent" onStop={handleDragStop}>
+        <Draggable
+          axis="y"
+          bounds="parent"
+          onDrag={handleDrag}
+          onStop={handleDragStop}
+        >
           <div
             className="absolute block md:hidden bottom-0 w-screen animate__animated animate__slideInUp bg-primary-white-100 transition-all duration-300 ease-in-out"
             style={{ height }}
