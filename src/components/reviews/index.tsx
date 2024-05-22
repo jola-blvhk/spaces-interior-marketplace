158657788bc110b7c62a3a710188dcd61029473d
jola-blvhk <awujoolabello@gmail.com>
2024-05-22 12:28:33 +0100

    reviews page

--- src/components/reviews/index.tsx
+++ src/components/reviews/index.tsx
@@ -44,18 +44,26 @@ const ReviewContainer = () => {
         ></div>
         <DraggableCore onDrag={handleDrag} onStop={handleDragStop}>
           <div
-            className="absolute block md:hidden bottom-0 w-screen px-5 py-8 animate__animated animate__slideInUp bg-primary-white-100 rounded-t-2xl transition-all duration-300 ease-in-out"
+            className="absolute block md:hidden bottom-0 w-screen px-6 py-8 animate__animated animate__slideInUp bg-primary-white-100 rounded-t-2xl transition-all duration-300 ease-in-out"
             style={{
               height,
               transform: `translate(${position.x}px, ${position.y}px)`,
             }}
           >
             <div className="fixed inset-x-0 top-1.5 m-auto bg-secondary-green-100/50 rounded-3xl w-[10%] h-1.5"></div>
-            <ReviewComponent />
+            <ReviewComponent
+              modalClose={() => {
+                dispatch(setReviewsComponentState(false));
+              }}
+            />
           </div>
         </DraggableCore>
-        <div className="hidden md:block absolute px-5 py-7 right-0 h-screen  md:w-[50%] lg:w-[35%] xl:w-[25%] animate__animated animate__slideInRight bg-primary-white-100">
-          <ReviewComponent />
+        <div className="hidden md:block absolute px-6 py-9 right-0 h-screen  md:w-[50%] lg:w-[35%] xl:w-[25%] animate__animated animate__slideInRight bg-primary-white-100">
+          <ReviewComponent
+            modalClose={() => {
+              dispatch(setReviewsComponentState(false));
+            }}
+          />
         </div>
       </div>
     </div>
--- src/components/reviews/reviewComponent.tsx
+++ src/components/reviews/reviewComponent.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { MdClose } from "react-icons/md";
 
-const ReviewComponent = () => {
+interface ReviewComponentProps {
+  modalClose: () => void;
+}
+const ReviewComponent: React.FC<ReviewComponentProps> = ({ modalClose }) => {
   return (
     <div>
-      <header className="flex justify-between items-center">
-        <h2 className="text-secondary-green-100 font-medium ">Reviews</h2>
+      <header className="flex justify-between items-center pb-4 md:pb-5 border-b-[0.46px] border-b-secondary-green-100/50">
+        <h2 className="text-secondary-green-100 text-lg md:text-xl font-medium ">
+          Reviews
+        </h2>
         <MdClose
           className=" text-primary-black-90 text-2xl"
-          onClick={() => {}}
+          onClick={modalClose}
         />
       </header>
     </div>
