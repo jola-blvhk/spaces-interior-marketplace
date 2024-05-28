0a7e993e5ab0b056e0f4bed6c4a009995d6a76cb
jola-blvhk <awujoolabello@gmail.com>
2024-05-28 10:52:48 +0100

    cart page

--- src/components/cart-page/cart-item.tsx
+++ src/components/cart-page/cart-item.tsx
@@ -37,7 +37,7 @@ const CartItem: React.FC<CartItemProps> = ({
 
   return (
     <div className="flex items-center text-primary-black-90 text-sm lg:text-base  gap-x-3 md:gap-x-6 ">
-      <div className="relative w-24 h-24 md:w-32 md:h-32 rounded-[8.18px] md:rounded-[15px]">
+      <div className="relative w-28 h-28 md:w-32 md:h-32 rounded-[8.18px] md:rounded-[15px]">
         <Image
           src={image ? image : ""}
           className="object-cover rounded-[11px] md:rounded-[15px]"
