623fde51c4c1c16e81f143e497d852bb63c77636
jola-blvhk <awujoolabello@gmail.com>
2024-05-28 08:52:33 +0100

    cart page

--- src/components/top-products-page/index.tsx
+++ src/components/top-products-page/index.tsx
@@ -86,6 +86,7 @@ const TopProductsPage = () => {
         <main className=" grid grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 justify-between gap-x-1.5 md:gap-x-7 lg:gap-x-10 gap-y-6 md:gap-y-12 lg:gap-y-14 mt-4 md:mt-10 ">
           {topProducts?.map((product) => (
             <ProductBox
+              id={product.id}
               key={product.id}
               price={product?.price}
               name={product?.name}
