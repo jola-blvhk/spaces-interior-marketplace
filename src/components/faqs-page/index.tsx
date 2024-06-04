7c3aeb3d428dfafb35a89f117c8d8e6eb45c7332
jola-blvhk <awujoolabello@gmail.com>
2024-06-04 13:26:06 +0100

    edit details

--- src/components/faqs-page/index.tsx
+++ src/components/faqs-page/index.tsx
@@ -5,10 +5,15 @@ const FaqsPage = () => {
   return (
     <div className="">
       <div className={`relative cursor-pointer w-full h-44 md:h-96 `}>
-        <h1 className="absolute z-20 bottom-0 pl-5 pb-5 lg:pl-10 lg:pb-10  text-center text-[#D9D9D9] font-medium  text-lg md:text-xl lg:text-3xl  tracking-wide">
-          Title
-        </h1>
-
+        <div className="absolute z-20 bottom-0 pl-5 pb-5 lg:pl-10 lg:pb-10 space-y-2 md:space-y-4">
+          <h2 className="font-semibold text-sm md:text-lg text-secondary-green-100">The FAQS</h2>
+          <h1 className=" font-semibold text-secondary-green-100  text-3xl md:text-4xl lg:text-6xl  ">
+            Help Center
+          </h1>
+          <p className="text-primary-black-90 text-sm md:text-lg">
+            Everything you need to know about us
+          </p>
+        </div>
         <div className="w-full h-full relative">
           <Image
             src={image}
